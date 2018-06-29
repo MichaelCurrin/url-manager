5be5902bf7ce72489df6320da1973d4d33474839
MichaelCurrin <michael.currin@gmail.com>
2018-06-29 21:59:55 +0200

    feature: Create etc directory with app.conf and default_data.py

---
+++ url_manager/etc/default_data.py
@@ -0,0 +1,21 @@
+"""
+Default data.
+"""
+
+FORMAT = [
+    'bookmarks',
+    'history',
+    'onetab',
+    'manual-single',
+    'manual-bulk'
+]
+
+BROWSERS = [
+    'chrome',
+    'chromium',
+    'safari',
+    'firefox',
+    'firefoxquantum'
+]
+
+SOURCES = ['home']
