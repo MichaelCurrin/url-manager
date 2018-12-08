e9bdeb7a9fa2b335bf800a268d3b2fb9d7b75716
MichaelCurrin <michael.currin@gmail.com>
2018-12-08 22:33:21 +0200

    docs: Update convert.py

--- url_manager/lib/convert.py
+++ url_manager/lib/convert.py
@@ -14,23 +14,24 @@ def from_chrome_time(chrome_epoch):
 
     Chrome stores timestamps for bookmark and history records in the
     Chrome epoch format.
-        See answer at: https://stackoverflow.com/questions/539900/google-bookmark-export-date-format
-            "Chrome uses a modified form of the Windows Time format
-            ("Windows epoch") for its timestamps, both in the Bookmarks file
-            and the history files. The Windows Time format is the number of
-            100ns-es since January 1, 1601. The Chrome format is the number of
-            microseconds since the same date, and thus 1/10 as large."
-        See also:
-            http://fileformats.archiveteam.org/wiki/Chrome_bookmarks
+
+    See answer at: https://stackoverflow.com/questions/539900/google-bookmark-export-date-format
+        "Chrome uses a modified form of the Windows Time format
+        ("Windows epoch") for its timestamps, both in the Bookmarks file
+        and the history files. The Windows Time format is the number of
+        100ns-es since January 1, 1601. The Chrome format is the number of
+        microseconds since the same date, and thus 1/10 as large."
+    See also:
+        http://fileformats.archiveteam.org/wiki/Chrome_bookmarks
 
     A Chrome epoch value can be converted to a unixtimestamp format using
     a formula described here in a blog post:
         http://linuxsleuthing.blogspot.co.za/2011/06/decoding-google-chrome-timestamps-in.html
 
-    @param chrome_epoch: Numeric value for Chrome epoch time, as int, float
-        or str.
+    :param chrome_epoch: Timestamp value in Chrome epoch format. As either
+        int, float or str.
 
-    @return: datetime.datetime object created from the input value.
+    :return: datetime.datetime object created from `chrome_epoch`.
     """
     chrome_epoch = float(chrome_epoch)
     unix_timestamp = chrome_epoch / 1000000 - 11644473600
@@ -42,11 +43,11 @@ def from_onetab_time(onetab_epoch):
     """
     Convert time from Onetab's epoch to Python datetime object.
 
-    @param onetab_epoch: Numeric value for the OneTab browser extension's
+    :param onetab_epoch: Numeric value for the OneTab browser extension's
         epoch time. This follows the unix timestamp standard, but in
         milliseconds.
 
-    @return: datetime.datetime object for the input value.
+    :return: datetime.datetime object created from `onetab_epoch`.
     """
     seconds = float(onetab_epoch) / 1000
 
