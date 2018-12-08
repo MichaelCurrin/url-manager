9ba93b99f7a201ab96b54f7873869f926bd9bde2
MichaelCurrin <michael.currin@gmail.com>
2018-12-08 22:30:02 +0200

    docs: Update convert.py

--- url_manager/lib/convert.py
+++ url_manager/lib/convert.py
@@ -1,28 +1,5 @@
 """
 Lib convert application file.
-
-The docs for Google bookmark time simply says it is recorded as milliseconds
-since the epoch. Further research on StackOverflow shows that the epoch here
-is NOT the unix timestamp epoch time.
-
-    https://stackoverflow.com/questions/539900/google-bookmark-export-date-format
-    "Chrome uses a modified form of the Windows Time format
-    ("Windows epoch") for its timestamps, both in the Bookmarks file
-    and the history files. The Windows Time format is the number of
-    100ns-es since January 1, 1601. The Chrome format is the number of
-    microseconds since the same date, and thus 1/10 as large."
-
-Also explained here:
-    http://fileformats.archiveteam.org/wiki/Chrome_bookmarks
-
-But, I had success with a formula provided in this blog post:
-    http://linuxsleuthing.blogspot.co.za/2011/06/decoding-google-chrome-timestamps-in.html
-
-    >>> chrome_epoch = 13169330714873550
-    >>> unix_timestamp = chrome_epoch / 1000000 - 11644473600
-    1524857114.8735504
-    >>> datetime.datetime.fromtimestamp(unix_timestamp)
-    datetime.datetime(2018, 4, 27, 21, 25, 14, 873550)
 """
 import datetime
 
@@ -33,7 +10,22 @@ DATETIME_FORMAT = "%Y-%m-%d %H:%M"
 
 def from_chrome_time(chrome_epoch):
     """
-    Convert time from Chrome's epoch format to a datetime object.
+    Convert timestamp from Chrome's epoch format to a datetime object.
+
+    Chrome stores timestamps for bookmark and history records in the
+    Chrome epoch format.
+        See answer at: https://stackoverflow.com/questions/539900/google-bookmark-export-date-format
+            "Chrome uses a modified form of the Windows Time format
+            ("Windows epoch") for its timestamps, both in the Bookmarks file
+            and the history files. The Windows Time format is the number of
+            100ns-es since January 1, 1601. The Chrome format is the number of
+            microseconds since the same date, and thus 1/10 as large."
+        See also:
+            http://fileformats.archiveteam.org/wiki/Chrome_bookmarks
+
+    A Chrome epoch value can be converted to a unixtimestamp format using
+    a formula described here in a blog post:
+        http://linuxsleuthing.blogspot.co.za/2011/06/decoding-google-chrome-timestamps-in.html
 
     @param chrome_epoch: Numeric value for Chrome epoch time, as int, float
         or str.
