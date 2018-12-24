5ca347a7e51740b46524dd08e07e915d93418ba2
MichaelCurrin <michael.currin@gmail.com>
2018-12-24 20:28:59 +0200

    docs: Update docstring

--- url_manager/lib/__init__.py
+++ url_manager/lib/__init__.py
@@ -1,8 +1,9 @@
 """
-Lib module initialisation file.
+Lib module initialization file.
 """
 import platform
 import os
+
 
 APP_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                        os.path.pardir))
